dc2cba65bc9b9da2dbc950f156e971d5c6f6193e
GermainArk <germain@arkham-studio.fr>
2026-01-23 13:40:49 +0100

    animation intro

--- game/scripts/project/javaScriptInEvents.js
+++ game/scripts/project/javaScriptInEvents.js
@@ -6,6 +6,12 @@ const scriptsInEvents = {
 	{
 		var rappers = ["eminem", "orelsan", "snoop", "wojtek", "ours", "canarticho", "lisa", "mike"];
 		runtime.setReturnValue(rappers[localVars.RapperIndex]);
+	},
+
+	async Rappers_Event11_Act1(runtime, localVars)
+	{
+		var rappers = ["b-rabbit", "orelsan", "snoooop", "wojtek", "l'ours", "canarticho", "lisa", "prison mike"];
+		runtime.setReturnValue(rappers[localVars.RapperIndex]);
 	}
 };
 
