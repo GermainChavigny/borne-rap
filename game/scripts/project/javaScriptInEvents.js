341552da8203eef987d7fa5547bb5571b45469f9
Germain <germain@arkham-studio.fr>
2026-01-23 21:37:45 +0100

    r2d2

--- game/scripts/project/javaScriptInEvents.js
+++ game/scripts/project/javaScriptInEvents.js
@@ -4,13 +4,13 @@ const scriptsInEvents = {
 
 	async Rappers_Event10_Act1(runtime, localVars)
 	{
-		var rappers = ["eminem", "orelsan", "snoop", "wojtek", "ours", "canarticho", "lisa", "mike"];
+		var rappers = ["eminem", "orelsan", "snoop", "wojtek", "ours", "canarticho", "lisa", "mike", "r2d2"];
 		runtime.setReturnValue(rappers[localVars.RapperIndex]);
 	},
 
 	async Rappers_Event11_Act1(runtime, localVars)
 	{
-		var rappers = ["b-rabbit", "orelsan", "snoooop", "wojtek", "l'ours", "canarticho", "lisa", "prison mike"];
+		var rappers = ["b-rabbit", "orelsan", "snoooop", "wojtek", "l'ours", "canarticho", "lisa", "prison mike", "r2d2"];
 		runtime.setReturnValue(rappers[localVars.RapperIndex]);
 	}
 };
